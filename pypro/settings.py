d4e0319e847cad45415cc9c55d198a61439a1e63
Fabrício Vale <fabriciovale18@gmail.com>
2021-05-16 14:36:48 -0300

    Inserido COLLECTFAST_STRATEGY

--- pypro/settings.py
+++ pypro/settings.py
@@ -138,11 +138,13 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = False
+    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_ENABLED = True
+    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'private'
 
     # static assets
     # --------------------------------------------------------------------
